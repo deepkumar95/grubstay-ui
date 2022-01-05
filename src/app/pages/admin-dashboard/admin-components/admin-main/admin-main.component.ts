2441752b0e75b5a61dcde379c38830899ae3d78a
Deep kumar <41341536+deepkumar95@users.noreply.github.com>
2022-01-05 12:33:42 +0530

    City Loaded Dynamically on Admin Home

--- src/app/pages/admin-dashboard/admin-components/admin-main/admin-main.component.ts
+++ src/app/pages/admin-dashboard/admin-components/admin-main/admin-main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CityServiceService } from '../../../../services/city-service.service';
+import { LocationService } from '../../../../services/location.service';
+import { SubLocationService } from '../../../../services/sub-location.service';
+import { CustomSnackBarService } from '../../../../services/helper/custom-snack-bar.service';
 
 @Component({
   selector: 'app-admin-main',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMainComponent implements OnInit {
 
-  constructor() { }
+  totalCitites = 0;
+  totalLocations = 0;
+  totalSubLocations = 0;
+
+  constructor(private _cityService: CityServiceService, private _locationService: LocationService, private _subLocationService: SubLocationService, private _snackBarService:CustomSnackBarService) { }
 
   ngOnInit(): void {
+    this.loadAllCity();
+    this.loadAllLocation();
+    this.loadAllSubLocation();
+  }
+  loadAllCity() {
+    this._cityService.loadAllCity().subscribe(
+      (response: any) => {
+        if (response.erorr && response.error != '') {
+          this._snackBarService.errorSnackBar("Something went wrong!...Please Try Again");
+          return;
+        } else {
+          let responseData:any = response.data;
+          if (responseData) {
+            this.totalCitites=responseData.length;
+          }
+        }
+      },
+      (error: any) => {
+        this._snackBarService.errorSnackBar("Something went wrong!...Please Try Again");
+        return;
+      });
+  }
+
+  loadAllLocation(){
+
+  }
+
+  loadAllSubLocation(){
+
   }
 
 }
