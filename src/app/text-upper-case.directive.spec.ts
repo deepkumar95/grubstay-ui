0de21adebf13c73b16f105a2503dde1feba83fc6
Kaviraj Singh Bhati <59439509+KavirajSinghBhati@users.noreply.github.com>
2023-04-04 22:16:27 +0530

    grubstay frontend

--- src/app/text-upper-case.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextUpperCaseDirective } from './text-upper-case.directive';
-
-describe('TextUpperCaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextUpperCaseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
